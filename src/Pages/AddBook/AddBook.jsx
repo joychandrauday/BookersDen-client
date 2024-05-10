43a4f786964d4d400046478d19d4010958a46847
joychandrauday <joychandraud@gmail.com>
2024-05-10 08:13:59 +0600

    set up add book form in frontend

--- src/Pages/AddBook/AddBook.jsx
+++ src/Pages/AddBook/AddBook.jsx
@@ -1,11 +1,199 @@
-import React from 'react';
+import Swal from "sweetalert2";
+import { AuthContext } from "./../../Provider/Provider";
+import { Helmet } from "react-helmet-async";
+import { useContext } from "react";
+import './style.css'
 
 const AddBook = () => {
-    return (
-        <div>
-            <h1 className="text-3xl text-center">add book</h1>
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const image = form.image.value;
+    const book_name = form.book_name.value;
+    const genre = form.genre.value;
+    const book_numbers = form.book_numbers.value;
+    const short_description = form.short_description.value;
+    const author_name = form.author_name.value;
+    const rating = form.rating.value;
+    const userEmail = form.userEmail.value;
+    const userName = form.userName.value;
+
+    const formData = {
+      image,
+      book_name,
+      genre,
+      book_numbers,
+      short_description,
+      author_name,
+      rating,
+      userEmail,
+      userName,
+    };
+    console.log(formData)
+    // send data to server
+    // fetch("https://travelpulseserver.vercel.app/destinations", {
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(formData),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     if (data.insertedId) {
+    //       Swal.fire({
+    //         position: "center",
+    //         icon: "success",
+    //         title: "Your destination has been added.",
+    //         showConfirmButton: true,
+    //       });
+    //       form.reset();
+    //     } else {
+    //       Swal.fire({
+    //         position: "center",
+    //         icon: "error",
+    //         title: "something went wrong..",
+    //         showConfirmButton: true,
+    //       });
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error:", error);
+    //     Swal.fire({
+    //       position: "center",
+    //       icon: "error",
+    //       title: "Something went wrong..",
+    //       showConfirmButton: true,
+    //     });
+    //   });
+  };
+
+  return (
+    <div className=" lg:py-32 addNewPage">
+      <Helmet>
+        <title>Add A New Book</title>
+      </Helmet>
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-extrabold capitalize text-center pb-8">
+          Add a New Destination
+        </h1>
+        <form
+          onSubmit={handleSubmit}
+          className="lg:flex gap-8"
+          encType="multipart/form-data"
+        >
+          <div className=" flex flex-col lg:w-1/2">
+            <label className="mb-2">Book Image URL:</label>
+            <input
+              type="text"
+              name="image"
+              className="input input-bordered mb-4"
+              placeholder="Enter Book Image URL"
+              required
+            />
+            <label className="mb-2">Book Name:</label>
+            <input
+              type="text"
+              name="book_name"
+              className="input input-bordered mb-4"
+              placeholder="Enter Book Name"
+              required
+            />
+            <label className="mb-2">Genre Of the Book:</label>
+            <select name="genre" className="input input-bordered mb-4" required>
+              <option value="">Select Genre</option>
+              <option value="fiction">Fiction</option>
+              <option value="non-fiction">Non-Fiction</option>
+              <option value="mystery">Mystery</option>
+              <option value="fantasy">Fantasy</option>
+              <option value="romance">Romance</option>
+              <option value="science-fiction">Science Fiction</option>
+              <option value="thriller">Thriller</option>
+              <option value="horror">Horror</option>
+              <option value="biography">Biography</option>
+              <option value="historical-fiction">Historical Fiction</option>
+              <option value="self-help">Self-Help</option>
+              <option value="poetry">Poetry</option>
+            </select>
+            <label className="mb-2">Copy Of Book:</label>
+            <input
+              type="number"
+              name="book_numbers"
+              className="input input-bordered mb-4"
+              placeholder="Enter Book numbers"
+              required
+            />
+            <label className="mb-2">Short book Description:</label>
+            <textarea
+              name="short_description"
+              className="input input-bordered mb-4"
+              placeholder="Enter Short Description"
+              required
+            ></textarea>
+            {/* <label className="mb-2">Average Cost:</label>
+            <input
+              type="number"
+              name="average_cost"
+              className="input input-bordered mb-4"
+              placeholder="Enter Average Cost"
+              required
+            /> */}
+          </div>
+          <div className="flex flex-col lg:w-1/2">
+            <label className="mb-2">Author name:</label>
+            <input
+              type="text"
+              name="author_name"
+              className="input input-bordered mb-4"
+              placeholder="Author name"
+              required
+            />
+            <label className="mb-2">Book Rating:</label>
+            <input
+              type="number"
+              name="rating"
+              className="input input-bordered mb-4"
+              placeholder="Enter Book's rating."
+              required
+            />
+            {/* <label className="mb-2">Total Visitors Per Year:</label>
+            <input
+              type="number"
+              name="totalVisitorsPerYear"
+              className="input input-bordered mb-4"
+              placeholder="Enter Total Visitors Per Year"
+              required
+            /> */}
+            <label className="mb-2">User Email:</label>
+            <input
+              type="email"
+              name="userEmail"
+              defaultValue={user.email}
+              className="input input-bordered mb-4"
+              readOnly
+            />
+            <label className="mb-2">User Name:</label>
+            <input
+              type="text"
+              name="userName"
+              defaultValue={user.displayName}
+              className="input input-bordered mb-4"
+              readOnly
+            />
+            <button
+              type="submit"
+              className="btn btn-primary self-center w-full"
+            >
+              Add The Book To Library.
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddBook;
